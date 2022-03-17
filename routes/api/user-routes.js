c78e9acaa406024954f993c95e35a99b36fae302
Mike <ampgzo2@gmail.com>
2022-03-17 09:18:57 -0400

    update user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -37,6 +37,8 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
+    // To insert data, we can use Sequalize's .create() method. Pass in key/value pairs.
+    // the sql command looks like this INSERT INTO users (username, email, password) VALUES ("Lernantino", "lernantino@gmail.com", "password1234");
     // expects {username: 'Learnantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
         username: req.body.username,
@@ -51,7 +53,27 @@ router.post('/', (req, res) => {
 });
 
 // PUT /api/users/1
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    // notice user.update. put request can update and replace data
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {});
