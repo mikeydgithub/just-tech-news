ac2807296941485ed2c690a34d4248f7a6d879d1
Mike <ampgzo2@gmail.com>
2022-03-17 17:11:11 -0400

    beforecreate and beforeupdate bcrypt hash for newuserdata and updateduserdata

--- models/User.js
+++ models/User.js
@@ -55,16 +55,19 @@ User.init(
     hooks: {
       // set up beforeCreate lifecycle "hook" functionalitys
       // saltround value of 10 - how much time is needed to calculate a single BCrypt has.
-      async beforeCreate(newUserData){
+      async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-    }
-  },
-  {
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
+    },
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
