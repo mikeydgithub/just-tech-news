03234dd3912dfa9c656864d258559e943c4d7e3e
Mike <ampgzo2@gmail.com>
2022-03-18 14:33:30 -0400

    edit typos

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,5 @@
-const { Model, Datatypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connections');
-const { init } = require('./User');
 
 // create our Post model
 class Post extends Model {}
@@ -9,38 +8,38 @@ class Post extends Model {}
 // define Post schema
 Post.init(
     {
-        id: {
-            type: Datatypes.INTEGEER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: Datatypes.STRING,
-            allowNull: false
-        },
-        post_url: {
-            type: Datatypes.STRING,
-            allowNull: false,
-            validate: {
-                isURL: true
-            }
-        },
-        user_id: {
-            type: Datatypes.INTEGEER,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      post_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isURL: true
         }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
     },
     {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post'
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
     }
-);
+  );
 
 // export expression to make the Post model accessible to other parts of the application
 module.exports = Post;
