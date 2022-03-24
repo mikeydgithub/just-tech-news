ce7b18ce8f5aedbf30c61c27990ea8b432190a85
mikeydgithub <ampgzo2@gmail.com>
2022-03-24 10:46:45 -0400

    add array to the post data and serialize it. render the homepage. update homepage.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,14 +29,26 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
+      // need the entire array of posts to be in a template. That also means to serialize the entire array.
       const posts = dbPostData.map(post => post.get({ plain: true }));
-
+      // pass a single post object into the homepage template
+      // the res.render() function is used to render a view and sends the rendered HTML string to the client
+      console.log(dbPostData[0])
+      // normal properities of render() include an id property and a title. now the only property it has access to is the posts array. handlebars.js has built in helpers that will allow for minimal logic and looping over an array.
       res.render('homepage', { posts });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+// render login handlebars
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,8 +17,22 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(require('./controllers/'));
+
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
